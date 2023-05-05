f0f67d548fa65b12cb7dbfee1e60d1857d4018de
Pierre Blaess <pierre.blaess@epita.fr>
2023-05-05 08:30:06 +0200

    Add pretty logs

--- src/client.py
+++ src/client.py
@@ -1,3 +1,11 @@
+##
+# Author(s)
+# - Pierre BLAESS
+# - Adrien LANGOU
+# - Aurélien REBOURG
+# - Lucas THESE
+##
+
 import numpy as np
 from Pyfhel import Pyfhel, PyCtxt
 
@@ -22,58 +30,100 @@ Candidates = [
 ]
 
 
+# Parse arguments
+# usage: `python3 client.py -R` to ask for the results
+#        `python3 client.py ` to add a vote 
 parser = argparse.ArgumentParser(
                     prog='CRIPY',
                     description='Votai')
 
 parser.add_argument('-R', '--result',
+                    help="Ask the authenticate server to get the results of the vote",
                     action='store_true', required=False)
 args = parser.parse_args()
 
+class Logger():
+    def __init__(self) -> None:
+        self.OKBLUE = '\033[94m'
+        self.OKGREEN = '\033[92m'
+        self.FAIL = '\033[91m'
+        self.ENDC = '\033[0m'
+
+        self.prompt = "[E-Voting Client]"
+
+    def info(self, text):
+        print(f"{self.OKBLUE}{self.prompt}{self.ENDC}",
+             f"{text}")
+
+    def ok(self, text):
+        print(f"{self.OKGREEN}{self.prompt}{self.ENDC}",
+             f"{text}")
+
+    def server_error(self, text):
+        print(f"{self.FAIL}{self.prompt}{self.ENDC}",
+              f"{text}")
+        exit(1)
+
+log = Logger()
 
 def get_results():
-    print("Checking results")
+    """! Ask the __authenticate__ server to get the results of the vote
+    @param None
+    @return Winner name
+    """
+    log.info("Asking the server for vote results")
     r = requests.get(
             url=f'http://{AUTH_SERVER_URL}:{AUTH_SERVER_PORT}/results'
         )
     
     if 'Error' in r.text:
-        print(r.text)
-        exit(1)
+        log.server_error(r.text)
+
 
     result = r.text[1:-1].split()
     result = [ int(r) for r in result ]
     winner = Candidates[result.index(max(result))]
-    print(f"Winner is : {winner}\n")
+    log.ok(f"Winner is : {winner}\n")
     for k, v in enumerate(Candidates):
-        print(f"{v} has {result[k]} vote(s).")
+        log.ok(f"{v} has {result[k]} vote(s).")
     
     return winner
 
 
 def user_inputs():
-    name = input("Enter your name > ")
+    """! Ask the user for inputs (name, vote)
+    @param None
+    @return name: str, vote: int
+    """
+    log.info("Please enter your name :")
+    name = input("> ")
+
     h = hashlib.new('sha256')
     h.update(name.encode())
     name = h.hexdigest()
 
-    print("Candidates value :")
+    log.info("Here are the candidates :")
     for k, v in enumerate(Candidates):
         print("-", k, v)
 
-    vote = int(input("Vote for > ")) % 4
+    log.info("Please enter a candidate key :")
+    vote = int(input("> ")) % 4
 
     return name, vote
 
 
 def auth_client():
+    """! Ask the __authentication__ server to authenticate the client and send the public key.
+    @param None
+    @return token: str, context: str, pkey: str
+    """
     r = requests.get(
         url=f'http://{AUTH_SERVER_URL}:{AUTH_SERVER_PORT}/pk?name={name}'
     )
 
     if "Error" in r.text:
-        print(r.text)
-        exit(1)
+        log.server_error(r.text)
+
 
     res = json.loads(r.text)
     
@@ -84,6 +134,11 @@ def auth_client():
 
 
 def encrypt_vote(HE, candidate_index):
+    """! Encrypt the vote vector using server public key and context.
+    @param HE  :  Server homogenous encryption object.
+    @param candidate_index  :  User vote index in the Candidates array.
+    @return s_cyphervalue  :  serialized encrypted vote vector 
+    """
     # Generate and encrypt data
     plainvalue = np.array([0] * 4096)
     if not args.result:
@@ -96,24 +151,28 @@ def encrypt_vote(HE, candidate_index):
     return s_cyphervalue
 
 
-def send_vote(encrypted_vote):
-    print('Vote in progress')
+def send_vote(encrypted_vote, token):
+    """! Send the user vote to the __addition__ server
+    @param encrypted_vote  :  User serialized and encrypted vote.
+    @param token  :  Client token used to authenticate to the server  
+    @return Boolean of success or Error
+    """
     r = requests.post(
             url=f'http://{ADDITION_SERVER_URL}:{ADDITION_SERVER_PORT}/vote',
             json={
-                'cyphervalue': s_cyphervalue.decode("cp437"),
+                'cyphervalue': encrypted_vote.decode("cp437"),
                 'token': token
             }
         )
 
     if ("Error" in r.text):
-        print(r.text)
-        exit(1)
+        log.server_error(r.text)
+
+    log.ok("Your vote has successfully been completed.")
     return True
 
 
 if __name__ == "__main__":
-
     # If the user ask for the results, ask and print them
     # Otherwise vote
     if args.result:
@@ -123,17 +182,16 @@ if __name__ == "__main__":
     # Ask user
     name, uservote = user_inputs()
 
+    # Ask Authenticate server
+    anonymoustoken, context, pkey = auth_client()
 
     # Generate Pyfhel Object
     HE = Pyfhel()
-
-    # Ask Authenticate server
-    token, context, pkey = auth_client()
 
     HE.from_bytes_context(context.encode("cp437"))
     HE.from_bytes_public_key(pkey.encode("cp437"))
 
     # Encrypt the vote value and send it to the server
     s_cyphervalue = encrypt_vote(HE, uservote)
-    send_vote(s_cyphervalue)
+    send_vote(s_cyphervalue, anonymoustoken)
 
